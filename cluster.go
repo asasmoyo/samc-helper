1f8aef0795e3e7acc291ca9e1ce9e43ecad8bd9d
Arba Sasmoyo <asasmoyo@outlook.com>
2018-01-04 20:55:26 +0700

    remove nodes cache

--- cluster.go
+++ cluster.go
@@ -5,20 +5,16 @@ import (
 	"io/ioutil"
 )
 
-// peerID to nodeID cache
-var nodes map[string]int
-
 // ResolveNodeID resolves ethereum peerID into nodeID
 func ResolveNodeID(peerID string) int {
-	if nodes == nil {
-		nodesContent, err := ioutil.ReadFile(nodesFile)
-		if err != nil {
-			return -1
-		}
-		err = json.Unmarshal(nodesContent, &nodes)
-		if err != nil {
-			return -1
-		}
+	var nodes = map[string]int{}
+	nodesContent, err := ioutil.ReadFile(nodesFile)
+	if err != nil {
+		return -1
+	}
+	err = json.Unmarshal(nodesContent, &nodes)
+	if err != nil {
+		return -1
 	}
 
 	nodeID, ok := nodes[peerID]
