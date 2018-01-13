4072bca1ea0986fe9d30243d63a80c530366950e
Arba Sasmoyo <asasmoyo@outlook.com>
2018-01-13 09:39:34 +0700

    add more log data

--- ipc.go
+++ ipc.go
@@ -85,7 +85,7 @@ func NewIPC(data map[string]int, toID string) (string, error) {
 
 	fileName := fmt.Sprintf("%d", time.Now().UnixNano())
 	path := computePath(filepath.Join("new", ipcPrefix+fileName))
-	log.Println("[SAMC-HELPER] writing new ipc at", path, "data", data)
+	log.Println("[SAMC-HELPER] writing new ipc from", nodeID, "at", path, "data", data)
 	err := ioutil.WriteFile(path, buf.Bytes(), os.ModePerm)
 	if err != nil {
 		return "", err
